037cf92c10a329c58ac57c21dcfb9e7528d76815
Matthew Clark <matthewclark@10-21-164-157.dynapool.wireless.nyu.edu>
2022-02-11 14:10:54 -0500

    added print

--- bot/main.py
+++ bot/main.py
@@ -43,6 +43,7 @@ async def on_ready():
 
 @bot.command(name='setsword')
 async def dosomething(ctx, data):
+    await ctx.send("command received")
     await writesingle("equipped_sword", "Diamond Sword")
 
 @bot.command()
